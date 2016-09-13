3e1237be130961e32126c276b033fc5599e29b32
lidacity <lidacity@users.noreply.github.com>
2016-09-13 09:55:31 +0300

    Delete gh-pages.sh

--- gh-pages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git checkout gh-pages
-git checkout master -- exquery-restxq-specification
-git commit -m "Merge in of specification from master branch"
-# git push #uncomment if you want to auto-push
-git checkout master
