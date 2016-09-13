b054049bbad38186b65c857d91c50ad9cf828a77
lidacity <lidacity@users.noreply.github.com>
2016-09-13 09:54:54 +0300

    предварительные изменения

--- README.md
+++ README.md
@@ -12,15 +12,20 @@
   https://docs.google.com/spreadsheets/d/1Uu7Ff1goNTzMB5ZT5qz5rWbHyad76I0WcuB7dtGkNrw/edit#gid=832950027
   http://overpass-turbo.eu/s/9Gm
   http://news.tut.by/society/398404.html
+  https://habrahabr.ru/sandbox/69352/
  как основные данные
-  Праект Карта горада Ліда. Частка "Іх імёнамі названы вуліцы"
+  Праект Карта горада Ліда. Частка "Іх імёнамі названы вуліцы", 2003-2005
   wikipedia.org
+  wikidata.org
   josm.openstreetmap.de
   openstreetmap.org
-  ubuntu + python3 + osmapi
+  python3 + osmapi + geojson + pywikibot
   Урбанонімы горада Ліды. В. Сліўкін "Лідскі летапісец" № 22, 23-2
   Улицы Лиды или лидские годонимы. Cтарший научный сотрудник Лидского музея Сливкин Валерий Васильевич. http://www.lida.info/ulicy-lidy-ili-lidskie-godonimy/
+ и другие...
 
+Увы, не все персоны представлены на wikidata, буду рад если про них создадут страницу на wikipedia, и я с удовольствием добавлю их в проект.
+Если вы обнаружили ошибку, есть замечание, или что-нибудь ещё, свяжитесь dzmitry@lidacity.by
 
 ## Технические подробности
 Для отрисовки карты используется библиотека [leaflet.js](http://leafletjs.com/) с рядом плагинов.
--- js/main.js
+++ js/main.js
@@ -1,55 +1,82 @@
-var map = L.map('map').setView([53.90, 25.30], 13);
- L.tileLayer.grayscale('http://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  { attribution: ' © <a href="http://www.openstreetmap.org/copyright">Openstreetmap</a> | © <a href="http://lidacity.by/street">LidaCity</a>',
-    maxZoom: 18,
-    minZoom: 11,
-    maxBounds: [[53.80, 25.20], [54.00, 25.40]]
-  }).addTo(map);
+// LidaCity, 2015-2016
 
-var streetsLayer = L.geoJson(streetsData.streets, {
- style: function (feature) {
-  var zoom = map.getZoom(),
-   style = streetsStyle[zoom];
-  return style[feature.properties.stylegroup];
- },
- onEachFeature: function (feature, layer) {
-  feature.layer = layer;
-   layer.bindPopup(
-    '<b>' + feature.properties.osm + '</b><hr />' +
-    '<i>' + feature.properties.tag + '</i>:<br/ >' +
-    '<a href="' + feature.properties.url + '" target="_blank" class="map-popup-link">' + feature.properties.name + '</a><hr />' +
-    '');//feature.properties.wiki);
-    
- }
-});
+var OptionMap = {
+ attribution: ' © <a href="http://www.openstreetmap.org/copyright">Openstreetmap</a> | © <a href="http://streets.lidacity.by/">LidaCity</a>',
+ maxZoom: 18,
+ minZoom: 11,
+ maxBounds: [[53.80, 25.20], [54.00, 25.40]]
+}
 
-map.on('zoomend', function() {
- streetsLayer.setStyle(function (feature) {
-  var zoom = map.getZoom(),
-   style = streetsStyle[zoom];
-  return style[feature.properties.stylegroup];
- });
-});
+var Map = L.map('map').setView([53.90, 25.30], 13);
+//http://tile.openstreetmap.org/{z}/{x}/{y}.png
+//http://wiki.openstreetmap.org/wiki/Tiles#Graphical_Map_Tiles
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', OptionMap).addTo(Map);
 
-// Add fuse search control
-var options = {
+//
+
+function GetStyle(feature)
+{
+  var Zoom = Map.getZoom();
+  var Style = StreetsStyle[Zoom];
+  return Style[feature.properties.StyleGroup];
+}
+
+//
+
+function JsonEachFeature(feature, layer)
+{
+ feature.layer = layer;
+ layer.bindPopup(
+  '<b>' + feature.properties.StreetRu + '</b><hr />' +
+  '<i>' + feature.properties.Category + '</i>:<br/ >' +
+  '<a href="https://ru.wikipedia.org/wiki/' + feature.properties.SiteLinkRu + '" target="_blank" class="map-popup-link">' + feature.properties.LabelRu + '</a><hr />' +
+  '<small>' + feature.properties.Note + '</small>');
+}
+
+var OptionStreetsLayer = {
+ style: GetStyle,
+ onEachFeature: JsonEachFeature
+}
+
+var StreetsLayer = L.geoJson(StreetsData, OptionStreetsLayer);
+
+//
+
+function ZoomEnd()
+{
+ StreetsLayer.setStyle(GetStyle);
+}
+
+Map.on('zoomend', ZoomEnd);
+
+//
+
+function OptionShowResultFct(feature, container)
+{
+ var props = feature.properties;
+ var LabelName = L.DomUtil.create('b', null, container);
+ LabelName.innerHTML = props.LabelRu;
+ container.appendChild(L.DomUtil.create('br', null, container));
+ container.appendChild(document.createTextNode(props.Category));
+ container.appendChild(L.DomUtil.create('br', null, container));
+ var StreetName = L.DomUtil.create('small', null, container);
+ StreetName.innerHTML = '<i>' + props.StreetRu + '</i>';
+}
+
+var OptionsFuse = {
  position: 'topright',
  title: 'Chercher',
  placeholder: 'Найти улицу',
  maxResultLength: 7,
  threshold: 0.2,
  showInvisibleStreets: false,
- showResultFct: function(feature, container) {
-  var props = feature.properties;
-  var name = L.DomUtil.create('b', null, container);
-  name.innerHTML = props.name;
-  container.appendChild(L.DomUtil.create('br', null, container));
-  container.appendChild(document.createTextNode(props.tag));
- }
+ showResultFct: OptionShowResultFct
 };
 
-var fuseSearchCtrl = L.control.fuseSearch(options);
-map.addControl(fuseSearchCtrl);
-fuseSearchCtrl.indexStreets(streetsData.streets, ['name', 'osm', 'tag']);
+var fuseSearchCtrl = L.control.fuseSearch(OptionsFuse);
+Map.addControl(fuseSearchCtrl);
+fuseSearchCtrl.indexStreets(StreetsData.features, ['LabelRu', 'StreetRu', 'Category']);
 
-streetsLayer.addTo(map);
+//
+
+StreetsLayer.addTo(Map);
