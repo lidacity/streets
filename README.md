ba757d1e6a01b35829bf87a3b6dfc3d67fd96fb5
dzmitry <dzmitry@lidacity.by>
2018-05-22 13:59:08 +0300

    актуалізацыя да дадзеных, і новы варыянт працы

--- README.md
+++ README.md
@@ -3,28 +3,31 @@
 Город Лида. Их именами названы улицы
 Проект преобразован и оптимизирован для города Лиды, Беларусь.
 
-Использованы различные данные, а именно:
- как первоисточник 
-  http://habrahabr.ru/post/260893/
-  https://kogor.github.io//Streets-of-Valour-and-Victory/
-  https://github.com/KoGor/Streets-of-Valour-and-Victory
- как изучение
-  https://docs.google.com/spreadsheets/d/1Uu7Ff1goNTzMB5ZT5qz5rWbHyad76I0WcuB7dtGkNrw/edit#gid=832950027
-  http://overpass-turbo.eu/s/9Gm
-  http://news.tut.by/society/398404.html
-  https://habrahabr.ru/sandbox/69352/
- как основные данные
-  Праект Карта горада Ліда. Частка "Іх імёнамі названы вуліцы", 2003-2005
-  wikipedia.org
-  wikidata.org
-  josm.openstreetmap.de
-  openstreetmap.org
-  python3 + osmapi + geojson + pywikibot
-  Урбанонімы горада Ліды. В. Сліўкін "Лідскі летапісец" № 22, 23-2
-  Улицы Лиды или лидские годонимы. Cтарший научный сотрудник Лидского музея Сливкин Валерий Васильевич. http://www.lida.info/ulicy-lidy-ili-lidskie-godonimy/
+##Использованы различные данные, а именно:
+#### как первоисточник 
+* http://habrahabr.ru/post/260893/
+* https://kogor.github.io//Streets-of-Valour-and-Victory/
+* https://github.com/KoGor/Streets-of-Valour-and-Victory
+#### как изучение
+* https://docs.google.com/spreadsheets/d/1Uu7Ff1goNTzMB5ZT5qz5rWbHyad76I0WcuB7dtGkNrw/edit#gid=832950027
+* http://overpass-turbo.eu/s/9Gm
+* http://news.tut.by/society/398404.html
+* https://habrahabr.ru/sandbox/69352/
+* https://www.mediawiki.org/wiki/API:Main_page/ru
+* https://ru.wikipedia.org/wiki/Служебная:ApiSandbox#action=query&format=json&meta=siteinfo&siprop=namespaces
+#### как основные данные
+* Праект Карта горада Ліда. Частка "Іх імёнамі названы вуліцы", 2003-2005
+* wikipedia.org
+* wikidata.org
+* josm.openstreetmap.de
+* openstreetmap.org
+* python3 + osmapi + geojson + pywikibot + mv
+* Урбанонімы горада Ліды. В. Сліўкін "Лідскі летапісец" № 22, 23-2
+* Улицы Лиды или лидские годонимы. Cтарший научный сотрудник Лидского музея Сливкин Валерий Васильевич. http://www.lida.info/ulicy-lidy-ili-lidskie-godonimy/
  и другие...
 
 Увы, не все персоны представлены на wikidata, буду рад если про них создадут страницу на wikipedia, и я с удовольствием добавлю их в проект.
+
 Если вы обнаружили ошибку, есть замечание, или что-нибудь ещё, свяжитесь dzmitry@lidacity.by
 
 ## Технические подробности
--- js/main.js
+++ js/main.js
@@ -1,37 +1,118 @@
 // LidaCity, 2015-2016
 
-var OptionMap = {
+var OptionLayer = {
  attribution: ' © <a href="http://www.openstreetmap.org/copyright">Openstreetmap</a> | © <a href="http://streets.lidacity.by/">LidaCity</a>',
  maxZoom: 18,
  minZoom: 11,
  maxBounds: [[53.80, 25.20], [54.00, 25.40]]
 }
 
-var Map = L.map('map').setView([53.90, 25.30], 13);
+//var Map = L.map('map').setView([53.90, 25.30], 13);
 //http://tile.openstreetmap.org/{z}/{x}/{y}.png
 //http://wiki.openstreetmap.org/wiki/Tiles#Graphical_Map_Tiles
-L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', OptionMap).addTo(Map);
+//L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', OptionLayer).addTo(Map);
+
+var RU = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', OptionLayer);
+var BE = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', OptionLayer);
+
+var BaseMaps = {
+ "Русский язык": RU,
+ "Беларуская мова": BE
+};
+
+var OptionMap = {
+ center: [53.90, 25.30],
+ zoom: 13,
+ layers: [RU]
+}
+
+var Map = L.map('map', OptionMap);
 
 //
 
 function GetStyle(feature)
 {
   var Zoom = Map.getZoom();
-  var Style = StreetsStyle[Zoom];
-  return Style[feature.properties.StyleGroup];
+  var StyleGroup = feature.properties['StyleGroup'];
+  var Style = StreetsStyle[StyleGroup];
+  Style["fill"] = false;
+  Style["lineCap"] = "butt";
+  Style["lineJoin"] = "round";
+  Style["weight"] = Zoom - 9;
+  Style["opacity"] = (20 - Zoom) / 10;
+  return Style;
 }
 
-//
 
 function JsonEachFeature(feature, layer)
 {
  feature.layer = layer;
+ //
+ var Name = "";
+ if (feature.properties['ru']['Description'] != null)
+ {
+  Name = "<i>" + feature.properties['ru']['Description'] + "</i>:<br/ >";
+  if (feature.properties['ru']['SiteLink'] == null)
+   Name += "<font color='#DDD'><del>нет данных</del></font>";
+  else
+   Name += "<a href='https://ru.wikipedia.org/wiki/" + feature.properties['ru']['SiteLink'] + "' target='_blank' class='map-popup-link'>" + feature.properties['ru']['Label'] + "</a>";
+  Name += "<hr width='300' />";
+ }
+ //
+ var Note;
+ if (feature.properties['Note'] == null)
+  Note = "";
+ else
+  Note = "<hr /><small>" + feature.properties['Note'] + "</small>";
+ //
  layer.bindPopup(
-  '<b>' + feature.properties.StreetRu + '</b><hr />' +
-  '<i>' + feature.properties.Category + '</i>:<br/ >' +
-  '<a href="https://ru.wikipedia.org/wiki/' + feature.properties.SiteLinkRu + '" target="_blank" class="map-popup-link">' + feature.properties.LabelRu + '</a><hr />' +
-  '<small>' + feature.properties.Note + '</small>');
+  "<b>" + feature.properties['ru']['Name'] + "</b><hr />" +
+  Name +
+  "<div id='wiki'></div>" +
+  Note);
 }
+
+
+function Slice(Text)
+{
+ var Length = 256;
+ if (Text.length > Length)
+  return Text.slice(0, Length) + "…";
+ else
+  return Text;
+}
+
+function onPopupOpenClick(e)
+{
+ var queryWikipediaOption = {
+  action: 'query',
+  format: 'json',
+  prop: 'pageimages|extracts',
+  pithumbsize: '200',
+  //exsentences: '2',
+  exintro: '',
+  explaintext: '',
+  formatversion: '2',
+  titles: e.layer.feature.properties['ru']['SiteLink']
+ };
+ var queryWikipedia = mw.api.query('https://ru.wikipedia.org/w/api.php', queryWikipediaOption);
+ queryWikipedia(function (x) {
+  var Result = x.query.pages[0];
+  //console.log('mw', Result);
+  var Text = document.getElementById('wiki');
+  if (Result.thumbnail != null)
+   Img = "<img alt='" + Result.title + "' src='" + Result.thumbnail.source + "' />";
+  else
+   Img = "";
+  if (Result.extract != "")
+   Text.innerHTML = Img + "<br />" + Slice(Result.extract);
+  else
+   Text.innerHTML = "<font color='#DDD'><del>в wiki отсутствует информация</del></font>";
+ });
+
+}
+
+
 
 var OptionStreetsLayer = {
  style: GetStyle,
@@ -39,6 +120,17 @@ var OptionStreetsLayer = {
 }
 
 var StreetsLayer = L.geoJson(StreetsData, OptionStreetsLayer);
+StreetsLayer.on('popupopen', onPopupOpenClick);
+
+var List = L.layerGroup([StreetsLayer]).addTo(Map);
+
+var OverlayMaps = {
+    "Их именами названы улицы": List
+};
+
+L.control.layers(BaseMaps, OverlayMaps).addTo(Map);
+
+//StreetsLayer.addTo(Map);
 
 //
 
@@ -53,14 +145,13 @@ Map.on('zoomend', ZoomEnd);
 
 function OptionShowResultFct(feature, container)
 {
- var props = feature.properties;
  var LabelName = L.DomUtil.create('b', null, container);
- LabelName.innerHTML = props.LabelRu;
+ LabelName.innerHTML = feature.properties['ru']['Label'];
  container.appendChild(L.DomUtil.create('br', null, container));
- container.appendChild(document.createTextNode(props.Category));
+ container.appendChild(document.createTextNode(feature.properties['ru']['Description']));
  container.appendChild(L.DomUtil.create('br', null, container));
  var StreetName = L.DomUtil.create('small', null, container);
- StreetName.innerHTML = '<i>' + props.StreetRu + '</i>';
+ StreetName.innerHTML = '<i>' + feature.properties['ru']['Name'] + '</i>';
 }
 
 var OptionsFuse = {
@@ -69,14 +160,11 @@ var OptionsFuse = {
  placeholder: 'Найти улицу',
  maxResultLength: 7,
  threshold: 0.2,
- showInvisibleStreets: false,
+ showInvisibleFeatures: false,
  showResultFct: OptionShowResultFct
 };
 
 var fuseSearchCtrl = L.control.fuseSearch(OptionsFuse);
 Map.addControl(fuseSearchCtrl);
-fuseSearchCtrl.indexStreets(StreetsData.features, ['LabelRu', 'StreetRu', 'Category']);
-
-//
-
-StreetsLayer.addTo(Map);
+//fuseSearchCtrl.indexFeatures(StreetsData.features, ['ru.Label', 'ru.Name', 'ru.Description']);
+fuseSearchCtrl.indexFeatures(StreetsData.features, ['popupContent']);
