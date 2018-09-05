1cbe00bf3f5e15aedb36794798d673ff4f7e9363
dzmitry <dzmitry@lidacity.by>
2018-09-05 13:55:50 +0300

    розныя змены 2

---
+++ js/main.be.js
@@ -0,0 +1,6 @@
+// LidaCity, 2018
+var Lang = "be";
+var About = "Іх імёнамі названы вуліцы";
+var Address = "г. Ліда, Беларусь";
+var NotFound = "няма дадзеных";
+var NotWiki = "у wiki адсутнічае даведка";
