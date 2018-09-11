b4789034a57bedc7bb8294dd6a180424e5fc39d8
dzmitry <dzmitry@lidacity.by>
2018-09-11 15:36:38 +0300

    падтрымка дзьвух моў з адной старонкі

--- js/main.ru.js
+++
@@ -1,6 +0,0 @@
-// LidaCity, 2018
-var Lang = "ru";
-var About = "Их именами названы улицы";
-var Address = "г. Лида, Беларусь";
-var NotFound = "нет данных";
-var NotWiki = "в wiki отсутствует информация";
